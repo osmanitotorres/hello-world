2e1d45d36a43c31dbd2f0cdb1acd4a3d3ab2f5e2
Osmanito Torres <osmanitobrito@gmail.com>
2022-04-12 17:23:13 -0300

    servidor gin

--- hello.go
+++ hello.go
@@ -2,12 +2,26 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func Index(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"nome":  "Osmanito Torres",
+		"email": "osmanitobrito@gmail.com",
+		"site":  "www.osmanito.com",
+		"fone":  "(63)991127568",
+		"zap":   "(63)991127568",
+	})
+}
+
+func ShowNome(c *gin.Context) {
+	nome := c.Params.ByName("nome")
+	c.JSON(200, gin.H{
+		"Nome: ": nome,
+	})
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello world!",
-		})
-	})
+	router.GET("/", Index)
+	router.GET("/:nome", ShowNome)
 	router.Run()
 }
